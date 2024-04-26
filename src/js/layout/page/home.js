7acf5a721e27aad0359783110b0f3d9484678368
Max V Grushin <support@ids.spb.ru>
2024-04-26 14:01:52 +0300

    feat: добавлена секция брэнды

--- src/js/layout/page/home.js
+++ src/js/layout/page/home.js
@@ -87,3 +87,38 @@ new Swiper("#best-offers .swiper", {
     // type: 'custom',
   },
 });
+
+new Swiper("#brands .swiper", {
+  modules: [Pagination, Autoplay],
+
+  slidesPerView: 5.05,
+  spaceBetween: 20,
+  // loop: true,
+
+  speed: 2400,
+
+  autoplay: {
+    delay: 5000,
+    disableOnInteraction: false,
+  },
+
+  // breakpoints: {
+  //   768: {
+  //     slidesPerView: 3,
+  //     spaceBetween: 20
+  //   },
+  //   1200: {
+  //     slidesPerView: 3.5,
+  //     spaceBetween: 20
+  //   },
+  //   1400: {
+  //     slidesPerView: 4.8,
+  //     spaceBetween: 20
+  //   }
+  // },
+
+  pagination: {
+    el: '#brands .swiper-pagination',
+    clickable: true,
+  },
+});
